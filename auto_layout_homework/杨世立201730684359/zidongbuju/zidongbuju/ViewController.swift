a405dd6371ac8acd78eb766032fe7b3ecb78bb78
noobpythonuser <49983517+noobpythonuser@users.noreply.github.com>
2019-09-17 16:32:33 +0800

    Add files via upload

---
+++ auto_layout_homework/杨世立201730684359/zidongbuju/zidongbuju/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  zidongbuju
+//
+//  Created by Apple on 2019/9/17.
+//  Copyright © 2019 hg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
