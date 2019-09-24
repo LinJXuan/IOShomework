83ff5db0d995ab7527cdded69cfc3c346a36c600
caijunliang031 <38283189+caijunliang031@users.noreply.github.com>
2019-09-24 16:29:30 +0800

    Add files via upload

---
+++ login_view_homework/蔡俊亮201730681051/classNo3Tests/classNo3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  classNo3Tests.swift
+//  classNo3Tests
+//
+//  Created by clement on 2019/9/24.
+//  Copyright © 2019 clement. All rights reserved.
+//
+
+import XCTest
+@testable import classNo3
+
+class classNo3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
